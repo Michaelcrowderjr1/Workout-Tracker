95ffafa263c953fb747f3194d84ece07df7aca84
Michaelcrowderjr1 <70956054+Michaelcrowderjr1@users.noreply.github.com>
2021-02-17 19:57:20 -0500

    Create workout.js

---
+++ models/workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+	// creates a day attribute
+	day: {
+		type: Date,
+		default: Date.now
+	},
+	// Exercises definition
+	exercises: [
+		{
+			// Defines type attribute
+			type: {type: String},
+
+			// Defines name attribute
+			name: {type: String},
+
+			// Defines duration attribute
+			duration: {type: Number},
+
+			// Defines weight attribute
+			weight: {type: Number},
+
+			// Defines distance attribute
+			distance: {type: Number},
+
+			// Defines reps attribute
+			reps: {type: Number},
+
+			// Defines sets attribute
+			sets: {type: Number}
+		},
+	],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
